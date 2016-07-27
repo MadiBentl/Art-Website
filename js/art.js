486a4940b39db4ef2e3bd1d9f4482bc2a4148488
MadiBentley <mb.madisonbentley@gmail.com>
2016-07-27 12:40:21 -0700

    Get scroll to fixed navbar working

--- js/art.js
+++ js/art.js
@@ -1,2 +1,13 @@
-$(document).ready(function(){
+$(window).load(function(){
+	var  mn = $("#navbar");
+    mns = "navbar-scrolled";
+    hdr = $('#splash').height();
+
+	$(window).scroll(function() {
+		if( $(window).scrollTop() > hdr ) {
+		    mn.addClass(mns);
+		} else {
+		    mn.removeClass(mns);
+		}
+	});
 });
