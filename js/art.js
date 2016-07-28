511b83424d97d13007d8830b18d5099a1eb916fd
MadiBentley <mb.madisonbentley@gmail.com>
2016-07-28 13:02:40 -0700

    Add map marker

--- js/art.js
+++ js/art.js
@@ -13,8 +13,15 @@ $(window).load(function(){
 });
 var map;
 function initMap() {
+	var myLatLng = {lat: 49.283, lng: -123.120};
     map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: 49.283, lng: -123.120},
+        scrollwheel: false,
         zoom: 6
     });
+    var marker = new google.maps.Marker({
+          position: myLatLng,
+          map: map,
+          title: 'Hello World!'
+    })
 }
